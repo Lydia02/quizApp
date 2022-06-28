2fe7040dbcccabe25e565052577b563976ad4168
Kwame O <104514687+stardev1@users.noreply.github.com>
2022-06-28 20:46:25 +0000

    Add question.js file

---
+++ question.js
@@ -0,0 +1,123 @@
+const quiz = [
+
+{
+    q: "The correct sequence of HTML tags for starting a webpage is",
+    options: ["Head, Title, HTML, body", "HTML, Body, Title, Head", "HTML, Head, Title, Body", "HTML, Head, Title, Body"],
+    answer: 3
+},
+
+{
+    q: "Which of the following element is responsible for making the text bold in HTML?",
+    options: ["<pre>", "<a>", "<b>", "<br>"],
+    answer: 2
+},
+
+{
+    q: "<input> is -",
+    options: ["a format tag.", "an empty tag.", "All of the above", "None of the above"],
+    answer: 1
+},
+
+{
+    q: "Which of the following tag is used to make the underlined text?",
+    options: ["<i>", "<ul>", "<u>", "<pre>"],
+    answer: 2
+},
+
+{
+    q: "Which of the following attribute is used to provide a unique name to an element?",
+    options: ["class", "id", "type", "None of the above"],
+    answer: 1
+},
+
+{
+    q: "The property in CSS used to change the background color of an element is -",
+    options: ["bgcolor", "color", "background-color", "All of the above"],
+    answer: 2
+},
+
+{
+    q: "Are negative values allowed in the padding property?",
+    options: ["Yes", "No", "Can't say", "May be"],
+    answer: 1
+},
+
+{
+    q: "The CSS property used to specify the transparency of an element is -",
+    options: ["opacity", "filter", "visibility", "overlay"],
+    answer: 0
+},
+
+{
+    q: "Which of the following syntax is correct in CSS to make each word of a sentence start with a capital letter?",
+    options: ["text-style : capital;", "transform : capitalize;", "text-transform : capital;", "text-transform : capitalize;"],
+    answer: 3
+},
+
+{
+    q: "Which of the following is the correct syntax to select all paragraph elements in a div element?",
+    options: ["div p", "p", "div#p", "div ~ p"],
+    answer: 0
+},
+
+{
+    q: "When an interpreter encounters an empty statement, what will it do?",
+    options: ["Shows a warning", "Prompts to complete the statement", "Throws an error", "Ignores the statements"],
+    answer: 3
+},
+
+{
+    q: "'function' and 'var' are known as:",
+    options: ["Keywords", "Data types", "Declaration statements", "Prototypes"],
+    answer: 2
+},
+
+{
+    q: "When there is an indefinite or an infinite value during an arithmetic computation in a program, then JavaScript prints______.",
+    options: ["Prints an exception error", "Prints an overflow error", "Displays 'Infinity'", "Prints the value as such"],
+    answer: 2
+},
+
+{
+    q: "In JavaScript, which one of the following is not considered an error?",
+    options: ["Syntax error", "Missing semicolon", "Division by zero", "Missing brackets"],
+    answer: 2
+},
+
+{
+    q: "In JavaScript, the x===y statement implies that:",
+    options: ["Both x and y are equal in value, type and reference address as well.", "Both are x and y are equal in value only.", "Both are not same at all.", "Both are equal in the value and data type."],
+    answer: 3
+},
+
+{
+    q: "Which is not an Internet protocol?",
+    options: ["STP", "FTP", "HTTP", "IP"],
+    answer: 0
+},
+
+{
+    q: "The main circuit board in a computer is the:",
+    options: ["Decoder", "Highlight", "Select", "Motherboard"],
+    answer: 3
+},
+
+{
+    q: "Which data structure stores data using key-value pairs?",
+    options: ["Hash tables", "Trees", "Linked lists", "Arrays"],
+    answer: 0
+},
+
+{
+    q: "One of these programming languages is a low-level language:",
+    options: ["Python", "Java", "Assembly", "Ruby"],
+    answer: 2
+},
+
+{
+    q: "A major concept in OOP that involves hiding complex logic and implementation from the user, where the user just needs to call methods or use attributes without needing to know how things work underneath the hood is called?",
+    options: ["Polymorphism", "Inheritance", "Secrecy", "Abstraction"],
+    answer: 3
+}
+
+]
