4b3cc0120a54d8286716ad2ffd8c928a044d46ea
Lydia Ojoawo <ojoawolydia@gmail.com>
2022-07-01 02:48:21 +0100

    new commit

--- question.js
+++ question.js
@@ -110,7 +110,7 @@ const quiz = [
 
 {
     q: "Before a computer can understand a program it must be..",
-    options: ["Translated into its machine code", "Translated into a high level language", "Translated into a low level language"],
+    options: ["Translated into binary", "Translated into a high level language", "Translated into a english language"],
     answer: 0
 },
 
